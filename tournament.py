b349c0c5768e9f5bab0c70d33f11fbfd383fa592
Benjamin Chislett <chislett.ben@gmail.com>
2025-03-29 17:00:56 -0400

    Better formatting and sort tournament results

--- tournament.py
+++ tournament.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 from tqdm import tqdm
 
-from strategies import Move, strategies
+from strategies import Move, strategies, format_strategy_name
 from simulate import Winner, simulate_many
 
 
@@ -30,18 +30,22 @@ def tournament(
                 result[Winner.PLAYER_1] / num_games_per_pairing * 100
             results.at[p1_strategy, p2_strategy] = p1_win_percentage
 
-    results.index.name = "Player 1 Strategy"
-    results.columns.name = "Player 2 Strategy"
+    score_p1 = results.mean(axis=1)
+    score_p2 = 100 - results.mean(axis=0)
+    avg_score = (score_p1 + score_p2) / 2
+    results["AverageWinRate"] = avg_score
+    sorted_index = avg_score.sort_values(ascending=False).index
+    results = results.loc[sorted_index, sorted_index]
 
     return results
 
 if __name__ == "__main__":
     strategy_names = list(strategies.keys())
-    n = 1000
+    n = 10000
     results = tournament(strategy_names, n)
-    # results.to_csv("tournament_results.csv")
+    results.to_csv("results.csv")
 
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(15, 13))
     plt.title("Tournament Results")
     ax = sns.heatmap(
         results,
@@ -51,9 +55,9 @@ if __name__ == "__main__":
         cbar=True,
         center=50,
         cbar_kws={'label': 'Win Percentage'},
-        xticklabels=results.columns,
-        yticklabels=results.index
+        xticklabels=list(map(format_strategy_name, results.columns)),
+        yticklabels=list(map(format_strategy_name, results.index))
     )
-    ax.set(xlabel="player 2 strategy", ylabel="player 1 strategy")
+    ax.set(xlabel="Player 2 Strategy", ylabel="Player 1 Strategy")
     ax.figure.tight_layout()
     plt.show()
