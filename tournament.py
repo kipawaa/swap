27fa3519a39022cc0d1ebef393a96cf4ccc12a5a
Benjamin Chislett <chislett.ben@gmail.com>
2025-03-30 10:07:50 -0400

    add parallelism to main loop

--- tournament.py
+++ tournament.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import multiprocessing
 
 from tqdm import tqdm
 
@@ -8,27 +9,36 @@ from strategies import Move, strategies, format_strategy_name
 from simulate import Winner, simulate_many
 
 
+def _simulate_matchup(args):
+    p1_strategy, p2_strategy, num_games = args
+    result = simulate_many(
+        strategies[p1_strategy],
+        strategies[p2_strategy],
+        num_games
+    )
+    p1_win_percentage = result[Winner.PLAYER_1] / num_games * 100
+    return p1_strategy, p2_strategy, p1_win_percentage
+
+
 def tournament(
     strategy_names: list[str],
     num_games_per_pairing: int
 ) -> pd.DataFrame:
     """Run a tournament between all strategies."""
+    tasks = [(p1, p2, num_games_per_pairing)
+             for p1 in strategy_names
+             for p2 in strategy_names]
     results = pd.DataFrame(index=strategy_names,
                            columns=strategy_names,
                            dtype=float)
 
-    progress_bar = tqdm(total = len(strategy_names)**2)
-    for p1_strategy in strategy_names:
-        for p2_strategy in strategy_names:
+    progress_bar = tqdm(total=len(tasks))
+    num_processes = multiprocessing.cpu_count() - 1
+    # num_processes = 1
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        for p1, p2, p1_win_percentage in pool.imap_unordered(_simulate_matchup, tasks):
+            results.at[p1, p2] = p1_win_percentage
             progress_bar.update()
-            result = simulate_many(
-                strategies[p1_strategy],
-                strategies[p2_strategy],
-                num_games_per_pairing
-            )
-            p1_win_percentage = \
-                result[Winner.PLAYER_1] / num_games_per_pairing * 100
-            results.at[p1_strategy, p2_strategy] = p1_win_percentage
 
     score_p1 = results.mean(axis=1)
     score_p2 = 100 - results.mean(axis=0)
